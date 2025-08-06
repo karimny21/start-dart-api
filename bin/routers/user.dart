cacdaf2b6a8627a02a263e6908324c883a522fb5
Narongsak Yeemareb <53810925+karimny21@users.noreply.github.com>
2025-08-06 22:51:27 +0700

    Update user.dart

--- bin/routers/user.dart
+++ bin/routers/user.dart
@@ -9,12 +9,5 @@ Router userRouter() {
   router.post('/', UserController.createUser);
   router.put('/<id>', UserController.updateUser);
   router.delete('/<id>', UserController.deleteUser);
-
-  // router.get('/users', UserController.getAllUsers);
-  // router.get('/users/<id>', UserController.getUserById);
-  // router.post('/users', UserController.createUser);
-  // router.put('/users/<id>', UserController.updateUser);
-  // router.delete('/users/<id>', UserController.deleteUser);
-
   return router;
 }
